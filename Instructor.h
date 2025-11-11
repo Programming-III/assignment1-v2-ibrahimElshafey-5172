a5fe8159c822d905e62737300031bd2d7a1256f1
ibrahimElshafey-5172 <ibrahimabdelshafy97@gmail.com>
2025-11-11 09:56:37 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,9 +1,29 @@
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
+#define PERSON.H;
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
+//#write Instructor class hereclass instructor :public person{
+    private:
+    string department;
+    int experienceYears;
+    
+    public:
+     void display(){
+    cout<<"department"<<""<<department<<""<<"ExperienceYears"<<""<<experienceYears<<endl;
+     }
+     instructor();
+     instructor(string department,int experienceYears);
+    
+     
+     ~instructor();
+     instructor::~instructor()
+{
+}
+
+     
+};
 
 
 
